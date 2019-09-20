f58c86c7abe5d4360b2472ad349e857a46c18102
Andy Shu <shu@immux.com>
2019-09-20 20:33:22 +0800

    Filter dependencies from test coverage

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -3,7 +3,14 @@ export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-c
 cargo +nightly build --verbose $CARGO_OPTIONS
 cargo +nightly test --lib --verbose $CARGO_OPTIONS
 
-grcov . --ignore-dir deps -t lcov > lcov.info
-genhtml -o tests/coverage-report/ --show-details --highlight --ignore-errors source --legend /tmp/lcov.info
+zip -0 ccov.zip `find . \( -name "libimmux*.gc*" \) -print`;
+grcov ccov.zip -t lcov \
+  --ignore-dir *github.com* \
+  --ignore-dir *libcore* \
+  --ignore-dir *rustc* \
+  --ignore-dir *liballoc* \
+  > lcov.info
+genhtml -o tests/coverage-report/ --show-details --highlight --ignore-errors source --legend lcov.info
 
 rm lcov.info
+rm ccov.zip
